5322ae95927dcc9f3524a63ccd2317008134d9a1
Daniel Barros <contato.danielbarros@gmail.com>
2026-05-07 15:07:02 -0300

    implementação da classe Main e inicialização

---
+++ projetoP1/src/main/java/br/upe/atividaderegex/Main.java
@@ -0,0 +1,33 @@
+package br.upe.atividaderegex;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        AnalisadorDeLogs analisador = new AnalisadorDeLogs("src/main/java/br/upe/atividaderegex/access.log", "Analise");
+
+        int opcao;
+        do {
+            System.out.println("\nMENU");
+            System.out.println("1 - Recursos grandes respondidos");
+            System.out.println("2 - Não respondidos");
+            System.out.println("3 - % de requisições por SO");
+            System.out.println("4 - Média das requisições POST");
+            System.out.println("0 - Sair");
+            System.out.print("Opção: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1 -> analisador.processarRecursosGrandes();
+                case 2 -> analisador.processarNaoRespondidosNovembro();
+                case 3 -> analisador.calcularPercentualPorSO();
+                case 4 -> analisador.calcularMediaPost2021();
+                case 0 -> System.out.println("Encerrando...");
+                default -> System.out.println("Opção inválida!");
+            }
+        } while (opcao != 0);
+
+        scanner.close();
+    }
+}
